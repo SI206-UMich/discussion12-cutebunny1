a98fdf4558d311b91fab246496f60f5ce5c3332d
Kripa Srinivasan <kripas@umich.edu>
2022-11-30 23:32:32 -0500

    added code we worked on in discussion

--- discussion_12.py
+++ discussion_12.py
@@ -16,9 +16,10 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, job_id INT, salary INT)") # specify the primary key
+    conn.commit()
 
-# ADD EMPLOYEE'S INFORMTION TO THE TABLE
+# ADD EMPLOYEE'S INFORMATION TO THE TABLE
 
 def add_employee(filename, cur, conn):
     #load .json file and read job data
@@ -27,30 +28,68 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    employee_data = json.loads(file_data)
+    for item in employee_data:
+        employee_id = int(item['employee_id'])
+        first_name = item['first_name']
+        last_name = item['last_name']
+        hire_date = item['hire_date']
+        job_id = int(item['job_id'])
+        salary = int(item['salary'])
+        cur.execute('INSERT OR IGNORE INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)', (employee_id, first_name, last_name, hire_date, job_id, salary)) # avoid adding the same data several times
+    conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute('SELECT Employees.hire_date, Jobs.job_title FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id')
+    job_hire_date = cur.fetchall()
+    print(job_hire_date)
+    conn.commit()
+
+    sorted_job_hire_date = sorted(job_hire_date, key=lambda x: x[0])
+    # print(sorted_job_hire_date[0])
+    return sorted_job_hire_date[0][1]
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute('SELECT Employees.first_name, Employees.last_name FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id WHERE Employees.salary > Jobs.max_salary OR Employees.salary < Jobs.min_salary')
+    invalid = cur.fetchall()
+    conn.commit()
+    print(invalid)
+    invalid_names = []
+    for item in invalid:
+        invalid_names.append(item[0] + ' ' + item[1])
+    return invalid_names
     pass
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
-    pass
+    cur.execute('SELECT Employees.salary, Jobs.job_title FROM Employees JOIN Jobs ON Employees.job_id = Jobs.job_id')
+    salary_data = cur.fetchall()
+    conn.commit()
+    print(salary_data)
+
+    salary_list = []
+    job_list = []
+    for item in salary_data:
+        salary_list.append(item[0])
+        job_list.append(item[1])
+    
+    plt.figure()
+    plt.scatter(job_list, salary_list)
+    plt.show()
 
 class TestDiscussion12(unittest.TestCase):
     def setUp(self) -> None:
         self.cur, self.conn = setUpDatabase('HR.db')
 
     def test_create_employee_table(self):
-        self.cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='employees'")
+        self.cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='Employees'")
         table_check = self.cur.fetchall()[0][0]
-        self.assertEqual(table_check, 1, "Error: 'employees' table was not found")
-        self.cur.execute("SELECT * FROM employees")
+        self.assertEqual(table_check, 1, "Error: 'Employees' table was not found")
+        self.cur.execute("SELECT * FROM Employees")
         count = len(self.cur.fetchall())
         self.assertEqual(count, 13)
 
